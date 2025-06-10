14fe6f980b0adc7dc9a1e1dd18b1f82b4256b1d9
dattlv <tlvdat.bkdn@gmail.com>
2025-06-10 22:04:09 +0700

    product list page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/products', function () {
+    return view('products');
+})->name('products.index');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/order', function () {
+    return view('order');
+})->name('order');
